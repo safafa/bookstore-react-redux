89dcd39b98b22f9c73e978ecd32030f7458fbac9
unknown <safaaballagh97@gmail.com>
2021-09-27 16:20:59 +0100

    Make form functional

--- src/components/form.js
+++ src/components/form.js
@@ -1,14 +1,27 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const Form = ({ addBook }) => (
-  <form onSubmit={() => addBook({ author: 'test', title: 'test' })}>
-    <input name="author" placeholder="author" />
-    <input name="title" placeholder="title" />
-    <input type="submit" value="Submit" />
-  </form>
-);
+const Form = ({ addBook }) => {
+  const [book, setBook] = useState({});
+
+  const handleChange = (event) => {
+    setBook({ ...book, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    addBook(book);
+    event.preventDefault();
+  };
+  return (
+    <form onSubmit={(event) => handleSubmit(event)}>
+      <input name="author" placeholder="author" value={book.author} onChange={(event) => handleChange(event)} />
+      <input name="title" placeholder="title" value={book.title} onChange={(event) => handleChange(event)} />
+      <input type="submit" value="Submit" />
+    </form>
+
+  );
+};
 
 Form.propTypes = { addBook: PropTypes.func.isRequired };
 
